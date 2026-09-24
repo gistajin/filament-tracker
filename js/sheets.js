18fa3ccec39bd5157b036c4ecfccc683885f1527
gistajin <gisalvi@gmail.com>
2026-09-24 00:23:41 -0500

    Update sheets.js

--- js/sheets.js
+++ js/sheets.js
@@ -76,7 +76,7 @@ const Sheets = {
       printed: String(r.printed ?? ''), toSell: String(r.toSell ?? ''), price: String(r.price ?? ''),
       sold: String(r.sold || '0'), photo: String(r.photo || ''), variant: String(r.variant || ''),
       colors: String(r.colors || ''), sortOrder: String(r.sortOrder ?? '0'),
-      photoFullUrl: String(r.photoFullUrl || '')
+      photoFullUrl: String(r.photoFullUrl || ''), publicCredit: !!r.publicCredit
     })).filter(r => r.id);
   },
 
